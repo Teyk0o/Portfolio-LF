c2578090038422d437af7be34a05ba8746b6ec53
Teyk0o <theovilainfr@gmail.com>
2024-01-16 12:39:37 +0100

    fix: astro config website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://minimma.vercel.app",
+  site: "https://www.liloufougeray.com/",
   image: {
     service: squooshImageService(),
   },
